2d75352075d339be81d0036d646e893fabd9bd7a
Albi Idrizi <albiidrizi27@gmail.com>
2024-10-31 22:47:49 +0100

    Service class added

---
+++ Data/Service.cs
@@ -0,0 +1,20 @@
+﻿namespace JobFinderAlbania.Data;
+
+public class Service
+{
+    public int Id { get; set; }
+    public string Name { get; set; } 
+    public string Description { get; set; }
+    public string Image { get; set; }
+    public double Price { get; set; }
+    
+    public string UserId { get; set; }
+    public virtual Seller User { get; set; }
+    
+    
+    public int DeliveryTime { get; set; } 
+    public int Revisions { get; set; }
+    public List<string> Tags { get; set; } = new List<string>();
+    public double Rating { get; set; }
+    
+}    
